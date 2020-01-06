6a4bfd401bb4e7e63f7a39f47db287ffe540064b
Oğuz Demirbaşcı <oguz@Oguz-MacBook-Pro.local>
2020-01-06 01:17:20 +0000

    pickle file export functions are added.

--- ir_eval/indexer.py
+++ ir_eval/indexer.py
@@ -11,6 +11,7 @@ import codecs
 import pprint
 import json
 import datetime
+import pickle
 
 class SrtIndexer:
   def __init__(self, path = os.path.dirname(os.path.abspath(__file__))):
@@ -22,7 +23,7 @@ class SrtIndexer:
     self.set_path(path)
     self.inverted_index = {}
     self.word2id = {}
-    self.json_name = "inverted_index.json"
+    self.json_name = "inverted_index.pickle"
     self.activate_stem = True
     self.activate_stop = True
     self.reindex = False
@@ -83,13 +84,15 @@ class SrtIndexer:
         it loads the old index file. Otherwise, It iterates all srt files, creates a new inverted index 
         and saves it.
     """
-    if os.path.isfile(self.json_name) and not self.reindex:
-      self.inverted_index = self.__json_load(self.json_name)
+    old_path = os.path.join(self.path, self.json_name)
+    if os.path.isfile(old_path) and not self.reindex:
+      self.inverted_index = self.__json_load(old_path)
+      print(sys.getsizeof(self.inverted_index))
     else:
       for element in self.__iterate_files():
           if (element.endswith('.srt')):
             self.__index_single_file(element)
-      self.__json_write(self.inverted_index, os.path.join(self.path, self.json_name))
+      self.__pickle_write(self.inverted_index, old_path)
 
   def __index_single_file(self, file_path):
     """ It indexes only one file. It is called for each file in build_index function.
@@ -97,18 +100,16 @@ class SrtIndexer:
     Arguments:
         file_path {string} -- the srt file path.
     """
+    file_name = os.path.splitext(os.path.basename(file_path))[0]; 
 
     with codecs.open(file_path, mode="r", encoding="utf-8", errors='ignore') as f:
       lines = f.readlines()
 
     data = self.__reformat_srt_file(lines)
-    
-    # Save data to db.
-
-    data = [preprocessing.preprocess(x, stemming=True, stop=False) for x in data if isinstance(x, str) ]
+    self.__save_to_db(data, file_name)
+    data = [preprocessing.preprocess(x[1], stemming=True, stop=False) for x in data]
     data = list(filter(None, data))
     self.__update_word_list(data)
-    file_name = os.path.splitext(os.path.basename(file_path))[0]; 
     self.__update_inverted_index(data, file_name)
     #String[] words = str.split("\s\n+");
 
@@ -132,13 +133,20 @@ class SrtIndexer:
         if temp_time == None:
           temp_time = start_time.timestamp()
       elif not pattern_numeric.match(line) and not pattern_empty.match(line):
-        temp += line.replace('\n', ' ').replace('...', '')
+        temp += line.replace('\n', ' ').replace('\r', '').replace('...', '')
         if pattern_end_sentence.match(line):
-          lines.append(temp_time)
-          lines.append(temp.strip())
+          temp = temp.strip()
+          if temp.startswith('-'):
+            temp = temp[1:]
+          lines.append([temp_time, temp])
           temp = ""
           temp_time = None
     return lines
+
+  def __save_to_db(self, data, title):
+    a = 0
+    #for line_number, sentence in enumerate(data, start=1):
+      #print(str(line_number) + ' ' + sentence[1])
 
   def __update_word_list(self, word_lists):
     """ It updates the word2id dictionary by adding new words in word_lists.
@@ -160,7 +168,7 @@ class SrtIndexer:
         file_name {string} -- the name of the indexed srt file, it is used to create document id. 
     """
     for word, id in self.word2id.items():
-      if id not in self.inverted_index.keys():
+      if word not in self.inverted_index.keys():
         #self.inverted_index[id] = dict()
         self.inverted_index[word] = dict()
 
@@ -194,7 +202,30 @@ class SrtIndexer:
     """
     return json.load(open(path))
 
-ab = SrtIndexer("/Users/oguz/Documents/ttds_movie_search/ir_eval/data/srt")
-ab.enforce_reindex(True)
+  def __pickle_write(self, data, path):
+    """ write data to path in pickle format.
+    
+    Arguments:
+        data {dictionary} -- the dictionary
+        path {string} -- path
+    """
+    with open(path, 'wb') as handle:
+      pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+  def __pickle_load(self, path):
+    """ It loads and returns a pickle data in dictionary structure.
+    
+    Arguments:
+        path {string} -- the path of the pickle data
+    
+    Returns:
+        dictionary -- the dictionary created from the pickle data.
+    """
+    with open(path, 'rb') as handle:
+      b = pickle.load(handle)
+    return b
+
+ab = SrtIndexer("/Users/oguz/Documents/ttds_movie_search/ir_eval/data/subtitles/")
+#ab.enforce_reindex(True)
 ab.build_index()
 #pprint.pprint(ab.get_inverted_index())
