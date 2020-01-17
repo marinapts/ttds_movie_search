3040c0e31c62316f8b066dc2fd8b4f6d23f9a769
Kasparas Masiukas <kmasiukas@gmail.com>
2020-01-17 11:56:57 +0000

    Added an interface for adding a database and developing methods for retrieving data from the database. Added Python "shelve" as persistent key-value storage and populated it with data from movies.json (using populate_movies_db.py script).

--- api/app.py
+++ api/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
 from flask_swagger_ui import get_swaggerui_blueprint
+from db.DB import get_db_instance
 import json
 
 app = Flask(__name__)
@@ -18,6 +19,8 @@ swaggerui_blueprint = get_swaggerui_blueprint(
 )
 app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
+db = get_db_instance()
+
 
 @app.route('/')
 def home():
@@ -34,10 +37,28 @@ def query_search():
     query_params = request.get_json()
 
     # @TODO: Get movies for the given query
-    with open('movies.json') as movies:
-        movies_dict = json.load(movies)
+    movies = db.get_movies_by_list_of_ids(['tt0111161',
+                                           'tt0068646',
+                                           'tt0468569',
+                                           'tt0071562',
+                                           'tt0167260',
+                                           'tt0110912',
+                                           'tt0108052',
+                                           'tt0050083',
+                                           'tt1375666',
+                                           'tt0137523',
+                                           'tt0120737',
+                                           'tt0109830',
+                                           'tt0060196',
+                                           'tt7286456',
+                                           'tt0167261',
+                                           'tt0133093',
+                                           'tt0099685',
+                                           'tt0080684',
+                                           'tt0073486',
+                                           'tt0056058'])
 
-    return json.dumps({'movies': movies_dict[:20]})
+    return json.dumps({'movies': movies})
 
 
 if __name__ == '__main__':
---
+++ api/db/ShelveDB.py
@@ -0,0 +1,36 @@
+import shelve, atexit, json
+from typing import List
+from db.DBInterface import DBInterface
+
+DB_PATH = 'db/data/shelve'
+
+class ShelveDB(DBInterface):
+
+    d = None
+
+    def __init__(self):
+        # Initialise dbm if it does not exist yet
+        c = shelve.open(DB_PATH, flag='c')
+        c.close()
+        # Now, open a read-only shelf.
+        self.d = shelve.open(DB_PATH, flag='r')
+        atexit.register(self.d.close)
+
+    def get_movies_by_list_of_ids(self, ids: List[str]):
+        return [self.d.get(id) for id in ids]
+
+    def populate_movies_data(self, file_path: str, clear: bool = False):
+        with shelve.open(DB_PATH, flag='c') as db_write_mode:
+            with open(file_path, 'r') as file:
+                ext = file_path.split('.')[-1]
+                if ext == 'json':  # JSON format - the whole file is a JSON list of movies
+                    movies = json.load(file)
+                    for movie in movies:
+                        db_write_mode[movie['id']] = movie
+                elif ext == 'jsonl':  # JSON Lines format - each line is a JSON object
+                    for line in file:
+                        movie = json.loads(line)
+                        db_write_mode[movie['id']] = movie
+                else:  # Invalid file extension. Do not populate
+                    print("Invalid file extension {}. Will not populate...".format(ext))
+
