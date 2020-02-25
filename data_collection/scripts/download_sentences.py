ea96e49db60244eb4f8a54bf3562a7e8d6598e97
Kasparas Masiukas <kmasiukas@gmail.com>
2020-02-25 16:25:35 +0000

    Improved find_term_counts.py script to run faster. Added a pickle file of term_counts for each movie id. Improved download_sentences.py script, now it will download them to a CSV file, containing sentence _ids and movie _ids.

--- data_collection/scripts/download_sentences.py
+++ data_collection/scripts/download_sentences.py
@@ -4,28 +4,36 @@ This script computes the average processed (stop words removed) sentence length 
 
 from db.MongoDB import MongoDB
 import io
+import csv
 
 # Change this to the number of sentences already processed if you want to resume the script.
 START_COUNT = 0
 
 mongo = MongoDB()
 sentences = mongo.sentences
-LIMIT = 100000
-output = io.open('sentences.txt', mode='a+', encoding='utf-8')
+LIMIT=100000
+output_file = io.open('sentences.csv', mode='a+', encoding='utf-8', newline='')
+output = csv.DictWriter(output_file, fieldnames=['_id', 'movie_id', 'sentence'])
+output.writeheader()
 
 total_processed = START_COUNT
 try:
     while True:
-        ss = sentences.find({}, {"sentence": 1, "_id": 0}, skip=total_processed, limit=LIMIT)
+        ss = sentences.find({}, {"sentence": 1, "_id": 1, 'movie_id': 1}, skip=total_processed)
         if ss is None:
             print("END OF SENTENCES REACHED!")
             break
 
         for s in ss:
-            output.write(f"{s['sentence']}\n")
+            output.writerow({
+                '_id': s['_id'],
+                'movie_id': s['movie_id'],
+                'sentence': s['sentence']
+            })
             total_processed += 1
+            if total_processed % LIMIT == 0:
+                print(f"{total_processed} sentences downloaded.")
 
-        output.flush()
         print("{} sentences have been downloaded so far".format(total_processed))
         if total_processed % LIMIT != 0:
             print("END OF SENTENCES REACHED!")
@@ -33,6 +41,4 @@ try:
 except:
     pass
 
-output.flush()
-output.close()
 print(f"Finished. If you want to resume the script, set START_COUNT to {total_processed}")
--- data_collection/scripts/find_term_counts.py
+++ data_collection/scripts/find_term_counts.py
@@ -20,27 +20,28 @@ LIMIT = 100000
 total_counted = START_COUNT
 # movie_term_counts = defaultdict(lambda: 0)
 movie_term_counts = pickle.load(open('movie_term_counts.p', 'rb'))
+movie_id = list(movie_term_counts.keys())[-1]
+movie_term_count = movie_term_counts[movie_id]
 try:
     while True:
-        ss = sentences.find({}, {"sentence": 1, "movie_id": 1, "_id": 0}, skip=total_counted, limit=LIMIT)
+        ss = sentences.find({}, {"sentence": 1, "movie_id": 1, "_id": 0}, skip=total_counted)
         if ss is None:
             break
-        batch_term_counts = defaultdict(lambda: 0)
         for s in ss:
+            if s['movie_id'] != movie_id:
+                movie_term_counts[movie_id] = movie_term_count
+                movie_id = s['movie_id']
+                movie_term_count = 0
             terms = set(preprocess(s['sentence']))
-            batch_term_counts[s['movie_id']] += len(terms)
+            movie_term_count += len(terms)
             total_counted += 1
             if total_counted % 10000 == 0:
-                print(f"Count: {total_counted}")
-
-        for movie in batch_term_counts.keys():
-            movie_term_counts[movie] += batch_term_counts[movie]
-
-        print("Count: {}\nTotal movies: {}\n".format(total_counted, len(movie_term_counts)))
+                print(f"Count: {total_counted}, Total movies: {len(movie_term_counts)}")
+        movie_term_counts[movie_id] = movie_term_count
         if total_counted % LIMIT != 0:
             break
 except:
     pass
 
-pickle.dump(dict(movie_term_counts), open(f'movie_term_counts{total_counted/100000}.p', 'wb'))
+pickle.dump(dict(movie_term_counts), open(f'movie_term_counts{int(total_counted/100000)}.p', 'wb'))
 print("Finished.")
