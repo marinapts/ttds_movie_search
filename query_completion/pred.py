a639b4d822bfed0183d4866e2d231d9845b3e8b8
Maysara <maysara.fayez@gmail.com>
2020-02-26 22:47:59 +0000

    Add files via upload

--- query_completion/pred.py
+++ query_completion/pred.py
@@ -5,10 +5,14 @@ import numpy as np
 import os
 
 #This model just predict one word only
-model_path = os.path.join('checkpoints', 'word_pred_Model6.h5')
+
+model_path = os.path.join('checkpoints', 'word_pred_Model2.h5')
 model = load_model(model_path)
-tokenizer = load(open('tokenizer_Model6','rb'))
-seq_len = 2 
+tokenizer = load(open('tokenizer_Model2','rb'))
+
+seq_len = 2
+num_gen_words = 1
+ 
 def gen_text(model, tokenizer, seq_len, seed_text, num_gen_words):
     output_text = []
     input_text = seed_text
@@ -28,5 +32,5 @@ while True:
     if seed_text.lower() == '--exit':
         break
     else:
-        out = gen_text(model, tokenizer, seq_len=seq_len, seed_text=seed_text, num_gen_words=3)
+        out = gen_text(model, tokenizer, seq_len=seq_len, seed_text=seed_text, num_gen_words=num_gen_words)
         print('Output: '+seed_text+' '+out)
