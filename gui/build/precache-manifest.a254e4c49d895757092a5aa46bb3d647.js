4949ea7bc8857d3523458b5bf49fa869022b4077
Leonie <l.bossemeyer@poste.de>
2020-02-26 16:37:36 +0000

    added build

---
+++ gui/build/precache-manifest.a254e4c49d895757092a5aa46bb3d647.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ee13744081d813b6c95fef8b1f26a9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd6a4893e6b59110a013",
+    "url": "/static/css/main.cb149616.chunk.css"
+  },
+  {
+    "revision": "380d1cfe11ca44e6cb92",
+    "url": "/static/js/2.d740bd21.chunk.js"
+  },
+  {
+    "revision": "358385828b55f8728566cc706231bf37",
+    "url": "/static/js/2.d740bd21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd6a4893e6b59110a013",
+    "url": "/static/js/main.cd42d41f.chunk.js"
+  },
+  {
+    "revision": "cb723d2747d95ec88c45",
+    "url": "/static/js/runtime-main.7f72db5d.js"
+  }
+]);
