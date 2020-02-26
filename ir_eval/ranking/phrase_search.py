aea84d103a526ed0803b117857348d9c2aefc6e9
Kasparas Masiukas <kmasiukas@gmail.com>
2020-02-26 15:59:52 +0000

    Reduced phrase search MAX_QUERY_TIME back to 10

--- ir_eval/ranking/phrase_search.py
+++ ir_eval/ranking/phrase_search.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 
 MAX_INDEX_SPLITS = 52  # maximum number of different entries in the inverted_index with the same term
 BATCH_SIZE = 52
-MAX_QUERY_TIME = 100  # max seconds to allow the query to run for
+MAX_QUERY_TIME = 10  # max seconds to allow the query to run for
 MIN_RATINGS = 100
 movie_ratings = defaultdict(lambda: MIN_RATINGS)
 try:
