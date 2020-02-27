51aa187ba6528fe1610268c11594ff9918272e2a
Oguz Demirbasci <ogguzx@gmail.com>
2020-02-27 02:29:31 +0000

    Revert "Quote match script added."
    This reverts commit d44d836b11515bfecb53f9d9c8322d9b62fdf526.

--- ir_eval/indexing/match_quotes.py
+++ ir_eval/indexing/match_quotes.py
@@ -2,10 +2,14 @@ import os
 import pandas 
 import sys
 from ir_eval import preprocessing 
-from csv import writer, reader, DictReader
+from csv import writer
 from joblib import Parallel, delayed, parallel_backend
 from tqdm import tqdm
-import codecs
+
+sentences = pandas.read_csv("/home/oguz/ttds_movie_search/ir_eval/data/sentences.csv")
+
+def get_sentences_by_movie(movie_id):
+    return sentences[sentences['movie_id'] == movie_id]
 
 def iterate_files(path):
     root = os.walk(path)
@@ -44,51 +48,40 @@ def get_similarity(str1, str2):
 
     return c / divider
 
+def find_most_similar(quote, movie_id):
+    movie_sentences = get_sentences_by_movie(movie_id)
+    similar_id = -1
+    max_similarity = 0.80
+    for row_index, row in movie_sentences.iterrows():
+        _id = row['_id']
+        sentence = row['sentence']
+        similarity = get_similarity(quote, sentence)
+        if similarity > max_similarity:
+            similar_id = _id
+            max_similarity = similarity
+        
+    return similar_id
+
 def append_to_csv(sentence_id, character):
     with open("quote_match.csv", 'a+', newline='') as write_obj:
         csv_writer = writer(write_obj)
         csv_writer.writerow([sentence_id, character])
 
-def similarity(quote_line, sentence):
+def match_quote(quote_line, movie_id):
     if ':' in quote_line:
         split = quote_line.replace("\n","").replace("\r", "").strip().split(':')
         character = split[0]
         quote = split[1]
-        return get_similarity(quote, sentence), character
-    else:
-        return 0, ''
+        sentence_id = find_most_similar(quote, movie_id)
+        if sentence_id > -1:
+            append_to_csv(sentence_id, character)
 
-def paralel_function(line, index):
-    sentence_id =  line[0]
-    movie_id = line[1]
-    sentence = line[2]
-    try:
-        if index > 0:
-            first3 = movie_id[2:5]
-            quote_path = "/home/oguz/ttds_movie_search/ir_eval/data/quotes/" + first3[0] + "/" + first3[1] + "/" + first3[2] + "/" + movie_id + ".txt"
-            with open(quote_path) as quote_file:
-                max_similarity = 0.80
-                character = ""
-                for line in quote_file:
-                    if line != "":
-                        sim, name = similarity(line, sentence)
-
-                        if sim > max_similarity:
-                            max_similarity = sim
-                            character = name
-                
-                if character != "":
-                    append_to_csv(sentence_id, character)
-    except:
-        print('Error occured')
-
-
-
-start = 0
-
-sentences = pandas.read_csv("/home/oguz/ttds_movie_search/ir_eval/data/sentences.csv", skiprows=start)
+def paralel_function(quote_path, movie_id):
+    with open(quote_path) as quote_file:
+        for line in quote_file:
+            match_quote(line, movie_id)
 
 
 with parallel_backend('multiprocessing', n_jobs=-1):
-    Parallel()(delayed(paralel_function)(row, i) for i, row in tqdm(sentences.iterrows(), total=77584425 - start))
+    Parallel()(delayed(paralel_function)(quote_path, movie_id) for quote_path, movie_id in tqdm(iterate_files("/home/oguz/ttds_movie_search/ir_eval/data/quotes"), total=218645))
  
---
+++ ir_eval/indexing/quotes.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import os
+import codecs
+
+from nltk import sent_tokenize
+import spacy
+#spacy_nlp = spacy.load('en_core_web_sm')   # Small model doesn't have any built in vector representations. It generates them by the inputs.
+spacy_nlp = spacy.load('en_core_web_md')    # Md model have a medium size of pre defined vectors.
+#spacy_nlp = spacy.load('en_core_web_lg')   # Lg model have a large size of pre defined vectors.
+
+class Quotes:
+  def __init__(self, quote_path = ''):
+    self.quotes = {}
+    if os.path.isfile(quote_path):
+      self.__read_quotes(quote_path)
+
+  def __read_quotes(self, quote_path):
+    """ It reads the quote file in given path. Uptades the quotes dictionary by adding character, sentences tuples. 
+    
+    Arguments:
+        quote_path {string} -- the quote path for a single quote file
+    """
+    with codecs.open(quote_path, mode="r", encoding="utf-8", errors='ignore') as f:
+      lines = f.readlines()
+    
+    for line in lines:
+      splitted = line.replace('\n','').replace('\r', '').split(': ')
+      if (len(splitted) > 1):
+        if (splitted[0] not in self.quotes.keys()):
+          self.quotes[splitted[0]] = ""
+        self.quotes[splitted[0]] += ' ' + splitted[1]
+
+    for character, sentences in self.quotes.items():
+      self.quotes[character] = sent_tokenize(sentences)
+
+
+  def find_character_name(self, sentence):
+    """ Finds the most similar sentence in the quotes and returns the character name who said the sentence.
+        Cosine similarity is used to find most similar sentence.
+    
+    Arguments:
+        sentence {string} -- input sentence found in subtitles.
+    
+    Returns:
+        string -- the character name
+    """
+    character_similarities = dict()
+    for character, docs in self.quotes.items():
+      character_similarities[character] = max([spacy_nlp(doc).similarity(spacy_nlp(sentence)) for doc in docs])
+    #print(character_similarities)
+    return max(character_similarities, key=character_similarities.get)
+
+
+print(spacy_nlp("You're not in school now. Miss Conway can't do anything.").similarity(spacy_nlp("You are not in school now  Conway cannot do anything.")))
