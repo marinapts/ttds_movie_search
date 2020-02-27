0514b3e726da5648dafc2dbff0805885f98063da
Kasparas Masiukas <kmasiukas@gmail.com>
2020-02-27 10:48:46 +0000

    Fixed clean_results()

--- api/app.py
+++ api/app.py
@@ -89,10 +89,7 @@ def filtering_keywords(query_results, filter_keywords):
 
 def clean_results(query_results):  # return only the fields we use for display on the webpage
     props = {'_id', 'movie_id', 'quote_id', 'time_ms', 'full_quote', 'title', 'categories', 'thumbnail'}
-    for result in query_results:
-        for key in result.keys():
-            if key not in props:
-                result.pop(key, False)
+    query_results = [{k: v for k, v in result.items() if k in props} for result in query_results]
     return query_results
 
 def filtering_title(query_results, filter_title):
